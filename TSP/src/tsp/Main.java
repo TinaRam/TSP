86ee10ba55888040d8e0405ec83e19ac71e6613d
TinaHodepina <31168143+TinaRam@users.noreply.github.com>
2019-09-19 20:01:46 +0200

    changed output

--- TSP/src/tsp/Main.java
+++ TSP/src/tsp/Main.java
@@ -4,21 +4,21 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("50 cities: \n");
+		System.out.println("\n50 cities:");
 		int[][] fifty = Graph.createGraph(50);
 		Graph.printGraph(fifty);
 
-		System.out.println("100 cities: \n");
+		System.out.println("\n100 cities:");
 		int[][] onehundred = Graph.createGraph(100);
 		Graph.printGraph(onehundred);
 
-		System.out.println("250 cities: \n");
+//		System.out.println("\n 250 cities:");
 		int[][] twofifty = Graph.createGraph(250);
-		Graph.printGraph(twofifty);
+//		Graph.printGraph(twofifty);
 
-		System.out.println("500 cities: \n");
+//		System.out.println("\n 500 cities:");
 		int[][] fivehundred = Graph.createGraph(500);
-		Graph.printGraph(fivehundred);
+//		Graph.printGraph(fivehundred);
 
 	}
 
